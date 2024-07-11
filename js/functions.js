404f4e785b4a411ae3981a4379f01501868e3302
Alexandra <alephanta@gmail.com>
2024-07-11 11:07:16 +0400

    Нужно больше функций-3

--- js/functions.js
+++ js/functions.js
@@ -1,63 +1,46 @@
 // Задача 1 - Функция для проверки длины строки -------------------------------------------------------------------------------------------------------------------------------------------------------
-const getLength = function (string, symbolsNumber) {
-  if (string.length <= symbolsNumber) {
-    return true;
-  } else {
-    return false;
-  }
-};
+const checkStringLength = (string, symbolsNumber) => string.length <= symbolsNumber;
 
-getLength('проверяемая строка', 20);
-getLength('проверяемая строка', 18);
-getLength('проверяемая строка', 10);
+checkStringLength('проверяемая строка', 20);
+checkStringLength('проверяемая строка', 18);
+checkStringLength('проверяемая строка', 10);
 
 // Задача 2 - Функция для проверки, является ли строка палиндромом -------------------------------------------------------------------------------------------------------------------------------
-const isPolindrom = function (string = '') {
-  // Привести строку к верхнему (метод toUpperCase()) или к нижнему (метод toLowerCase()) регистру
-  string = string.toUpperCase();
-  // записать полученную строку в новую переменную
-  const upperCaseString = string;
-  // Создать новую пустую строку и сохранить её в ещё одну переменную.
+const isPalindrom = (string) => {
+  const upperCaseString = string.toUpperCase();
   let newString = '';
-  // Создать цикл for, переменная-счётчик которого отсчитывает порядковые номера (индексы) символов в «нормализованной» строке с конца к началу
+
   for (let i = upperCaseString.length - 1; i >= 0; i--) {
-    // В теле цикла на каждой итерации получать очередной символ «нормализованной» строки (с помощью квадратных скобок или метода at()).
-    const currentSymbol = upperCaseString.at(i);
-    // С помощью оператора += дозаписать этот символ в созданную на втором шаге строку.
+    const currentSymbol = upperCaseString[i];
     newString += currentSymbol;
   }
 
-  // Сравнить
   return newString === upperCaseString;
 };
 
-isPolindrom('топот');
-isPolindrom('ДовОд');
-isPolindrom('Кекс');
+isPalindrom('топот');
+isPalindrom('ДовОд');
+isPalindrom('Кекс');
 
 // Задача 3 - Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN.
-const getNumber = function (string = '') {
-  // метод toString() — на случай, если в качестве параметра пришло число
-  string = string.toString();
-  // Новая пустая строка для цифр
+const parseNumber = (string) => {
   let newString = '';
-  // цикл — для перебора полученной строки
+
   for (let i = 0; i < string.length; i++) {
     const currentSymbol = string[i];
-    // проверить символ - является ли цифрой
+
     if (!isNaN(parseInt(currentSymbol, 10))) {
-      // добавить цифру к строке
       newString += currentSymbol;
     }
   }
-  // функция parseInt() — для превращения в число отдельных символов и результирующей строки
+
   const intValue = parseInt(newString, 10);
-  // функция Number.isNaN() — чтобы проверить, получилось ли превратить символ в число
+
   return isNaN(intValue) ? NaN : intValue;
 };
 
-getNumber('2023 год');
-getNumber('ECMAScript 2022');
-getNumber('1 кефир, 0.5 батона');
-getNumber('агент 007');
-getNumber('а я томат');
+parseNumber('2023 год');
+parseNumber('ECMAScript 2022');
+parseNumber('1 кефир, 0.5 батона');
+parseNumber('агент 007');
+parseNumber('а я томат');

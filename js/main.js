34df82fad0b6411c02fe318d630f2b53ac2f7c72
Alexandra <alephanta@gmail.com>
2024-08-25 19:15:49 +0400

    Открывается и закрывается

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 import { renderPictures } from './modules/renderPictures.js';
 import { createData } from './modules/dataPictures.js';
+import { addEventListenerToBigPicture, openBigPictureModal } from './modules/fullSizePicture.js';
 
 const createdPictures = createData();
 renderPictures(createdPictures);
+
+openBigPictureModal(createdPictures[0]);
+
+addEventListenerToBigPicture(createdPictures);

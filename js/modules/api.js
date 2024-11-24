6669973ce33d30ca4ef77ff007ae9ad945d9ef4d
Alexandra <alephanta@gmail.com>
2024-11-24 21:32:03 +0400

    Надо подкачаться

---
+++ js/modules/api.js
@@ -0,0 +1,29 @@
+const BASE_URL = 'https://31.javascript.htmlacademy.pro/kekstagra';
+
+const Route = {
+  GET_DATA: '/data',
+};
+
+const Method = {
+  GET: 'GET',
+};
+
+const ErrorText = {
+  [Method.GET]: 'Не удалось загрузить данные. Попробуйте позже.',
+};
+
+const load = (route, method = Method.GET, body = null) =>
+  fetch(`${BASE_URL}${route}`, { method, body })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject(new Error(ErrorText[method]));
+    })
+    .catch((error) => {
+      throw error;
+    });
+
+const getData = () => load(Route.GET_DATA, Method.GET);
+
+export { getData };
